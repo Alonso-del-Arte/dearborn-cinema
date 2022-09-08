d2bf282797230d153f2f9ad3b1e14f833c6ec373
Alonso del Arte <alonso.delarte@gmail.com>
2022-09-07 23:50:58 -0400

    Stubs

--- src/test/java/com/dearborncinema/service/MockMovieService.java
+++ src/test/java/com/dearborncinema/service/MockMovieService.java
@@ -1,4 +1,41 @@
 package com.dearborncinema.service;
 
-public class MockMovieService {
+import com.dearborncinema.entity.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MockMovieService implements MovieService {
+
+    // TODO: Write tests for this
+    @Override
+    public List<Movie> getAllMovies() {
+        return null;
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public Movie getMovieByID(int id) {
+        return null;
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public Movie addMovie(Movie movie) {
+        return null;
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public Movie updateMovie(Movie movie) {
+        return null;
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public Movie deleteMovieByID(int id) {
+        return null;
+    }
+
 }
--- src/test/java/com/dearborncinema/service/MockMovieServiceTest.java
+++ src/test/java/com/dearborncinema/service/MockMovieServiceTest.java
@@ -1,4 +1,7 @@
 package com.dearborncinema.service;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MockMovieServiceTest {
 }
