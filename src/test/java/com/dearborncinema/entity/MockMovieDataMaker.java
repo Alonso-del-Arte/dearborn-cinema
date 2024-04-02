1ea6f650d2b10b34b8239c3082d0b6db74e05bfc
Alonso del Arte <alonso.delarte@gmail.com>
2024-04-01 21:00:36 -0400

    New class

---
+++ src/test/java/com/dearborncinema/entity/MockMovieDataMaker.java
@@ -0,0 +1,33 @@
+package com.dearborncinema.entity;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+public class MockMovieDataMaker {
+
+    // TODO: Write tests for this
+    public static String makeUpTitle() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public static MPAARating chooseRating() {
+        return MPAARating.UNRATED;
+    }
+
+    // TODO: Write tests for this
+    public static String makeUpRatingReason() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public static Duration chooseDuration() {
+        return Duration.ZERO;
+    }
+
+    // TODO: Write tests for this
+    public static LocalDate choosePremiereDate() {
+        return LocalDate.EPOCH;
+    }
+
+}
---
+++ src/test/java/com/dearborncinema/entity/MockMovieDataMakerTest.java
@@ -0,0 +1,18 @@
+package com.dearborncinema.entity;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MockMovieDataMakerTest {
+
+    @Test
+    void testMakeUpTitle() {
+        fail("HAVEN'T WRITTEN TEST YET");
+    }
+    
+}
