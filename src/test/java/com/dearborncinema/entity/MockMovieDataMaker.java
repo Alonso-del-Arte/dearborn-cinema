0f3afd22595527bbdc71be4647777107a48cac2f
Alonso del Arte <alonso.delarte@gmail.com>
2024-04-02 20:45:55 -0400

    Passing makeUpTitle( ) test

--- src/test/java/com/dearborncinema/entity/MockMovieDataMaker.java
+++ src/test/java/com/dearborncinema/entity/MockMovieDataMaker.java
@@ -1,13 +1,59 @@
 package com.dearborncinema.entity;
 
+import static com.dearborncinema.entity.MovieTest.RANDOM;
+
 import java.time.Duration;
 import java.time.LocalDate;
 
 public class MockMovieDataMaker {
 
-    // TODO: Write tests for this
+    private static final String[] FRANCHISE_NAMES = {"Avengers",
+            "Back to the Present", "Cosmic Trek", "Cosmic Wars", "Dinosaurs",
+            "Elastic Man", "Fast Vehicles", "Girl Wizard", "Sherlock Holmes",
+            "Ionic Man", "Jokester", "Kern", "Lion Tamer", "Medal of Valor",
+            "Notung", "Outerborough", "Phantom Trapper", "Qualls of Destiny",
+            "Rural Detective", "Safe House", "Transforming Robots",
+            "Under the Moon", "Vowel Thief", "Wild North", "Xenon Morphemes",
+            "Young Sherlock Holmes", "Zoographer of Destiny"};
+
+    private static final int NUMBER_OF_FRANCHISES = FRANCHISE_NAMES.length;;
+
+    private static final String[] ROMAN_NUMERALS = {"I", "II", "III", "IV", "V",
+            "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV",
+            "XVI", "XVII", "XVIII", "XIX", "XX"};
+
+    private static final int NUMBER_OF_NUMERALS = ROMAN_NUMERALS.length;
+
+    private static final String[] GENERAL_NOUNS = {"Attack", "Battle",
+            "Convocation", "Defenestration", "Destruction", "Exodus", "Fall",
+            "Games", "Hindrance", "Illumination", "Journey", "Jog", "Karate",
+            "Loss", "March", "Narrative", "Opportunity", "Peroration",
+            "Questioning", "Return", "Revenge", "Revolt", "Rise", "Ruminations",
+            "Sojourn", "Temptation", "Unification", "Voyage", "Waters",
+            "Xylopyrography", "Youth", "Zenith"};
+
+    private static final int NUMBER_OF_GENERAL_NOUNS = GENERAL_NOUNS.length;
+
+    private static final String[] CHARACTER_NOUNS = {"Algernon", "the Beasts",
+            "the Clones", "the Dogs of War", "the Empire", "Frankenstein",
+            "Goldilocks", "Sherlock Holmes", "Hoth", "Indigo", "the Jedi",
+            "Don Juan", "Kilimanjaro", "the Penguins", "the Roses",
+            "the Serifs", "the Sith", "the Town Crier", "Underwood", "Voyager",
+            "Walker", "Xanadu", "Yonkers", "Zorro"};
+
+    private static final int NUMBERS_OF_CHARACTER_NOUNS = CHARACTER_NOUNS.length;
+
     public static String makeUpTitle() {
-        return "SORRY, NOT IMPLEMENTED YET";
+        String franchise
+                = FRANCHISE_NAMES[RANDOM.nextInt(NUMBER_OF_FRANCHISES)];
+        String romanNumeral
+                = ROMAN_NUMERALS[RANDOM.nextInt(NUMBER_OF_NUMERALS)];
+        String generalNoun
+                = GENERAL_NOUNS[RANDOM.nextInt(NUMBER_OF_GENERAL_NOUNS)];
+        String characterNoun
+                = CHARACTER_NOUNS[RANDOM.nextInt(NUMBERS_OF_CHARACTER_NOUNS)];
+        return franchise + " " + romanNumeral + ": The " + generalNoun + " of "
+                + characterNoun;
     }
 
     // TODO: Write tests for this
