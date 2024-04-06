6b89d2b0b7ce9a3ffbc550c7ad3201f59acf7a08
Alonso del Arte <alonso.delarte@gmail.com>
2024-04-05 22:05:07 -0400

    New class

---
+++ src/main/java/com/dearborncinema/utilities/TimeUtils.java
@@ -0,0 +1,13 @@
+package com.dearborncinema.utilities;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class TimeUtils {
+
+    // TODO: Write tests for this
+    public static LocalDate previous(LocalDate origin, DayOfWeek day) {
+        return origin;
+    }
+
+}
---
+++ src/test/java/com/dearborncinema/utilities/TimeUtilsTest.java
@@ -0,0 +1,19 @@
+package com.dearborncinema.utilities;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TimeUtilsTest {
+
+    public static final Random RANDOM = new Random();
+
+    @Test
+    void testPrevious() {
+        fail("HAVEN'T WRITTEN TEST YET");
+    }
+
+}
