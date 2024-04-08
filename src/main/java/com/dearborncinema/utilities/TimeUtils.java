eb8af299d37bc7b18bfa617270ff0c6a8ab3a57c
Alonso del Arte <alonso.delarte@gmail.com>
2024-04-08 01:07:43 -0400

    Refactored

--- src/main/java/com/dearborncinema/utilities/TimeUtils.java
+++ src/main/java/com/dearborncinema/utilities/TimeUtils.java
@@ -19,11 +19,17 @@ public class TimeUtils {
      * the previous Thursday from February 29, 2024 is February 22, 2024.
      */
     public static LocalDate previous(LocalDate origin, DayOfWeek day) {
-        LocalDate curr = origin;
-        do {
-            curr = curr.minusDays(1);
-        } while (curr.getDayOfWeek() != day);
-        return curr;
+        int originOrd = origin.getDayOfWeek().ordinal();
+        int targetOrd = day.ordinal();
+        int diff = originOrd - targetOrd;
+        int adjust = (diff <= 0) ? 7 : 0;
+        int daysToSubtract = diff + adjust;
+        return origin.minusDays(daysToSubtract);
+    }
+
+    // TODO: Write tests for this
+    public static LocalDate next(LocalDate origin, DayOfWeek day) {
+        return origin;
     }
 
 }
