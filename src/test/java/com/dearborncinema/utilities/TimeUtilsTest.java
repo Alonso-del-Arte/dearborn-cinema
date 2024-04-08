0413559bd51c6ac45794bf891b5a3dc46c4f39b9
Alonso del Arte <alonso.delarte@gmail.com>
2024-04-08 01:02:16 -0400

    Failing previous( ) test

--- src/test/java/com/dearborncinema/utilities/TimeUtilsTest.java
+++ src/test/java/com/dearborncinema/utilities/TimeUtilsTest.java
@@ -2,18 +2,37 @@ package com.dearborncinema.utilities;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Text;
 
 class TimeUtilsTest {
 
-    public static final Random RANDOM = new Random();
+    private static final Random RANDOM = new Random();
 
     @Test
     void testPrevious() {
-        fail("HAVEN'T WRITTEN TEST YET");
+        final int daysPerNonLeapYear = 365;
+        int numberOfYears = 9;
+        int bound = numberOfYears * daysPerNonLeapYear;
+        int daysToSubtract = RANDOM.nextInt(bound) + daysPerNonLeapYear;
+        LocalDate expected = LocalDate.now().minusDays(daysToSubtract);
+        DayOfWeek day = expected.getDayOfWeek();
+        int daysToAdd = RANDOM.nextInt(7) + 1;
+        LocalDate origin = expected.plusDays(daysToAdd);
+        LocalDate actual = TimeUtils.previous(origin, day);
+        DateTimeFormatter formatter
+                = DateTimeFormatter.ofPattern("EEE yyyy-MM-dd");
+        String msg = "Previous "
+                + day.getDisplayName(TextStyle.FULL, Locale.ROOT) + " from "
+                + origin.format(formatter) + " should be "
+                + expected.format(formatter);
+        assertEquals(expected, actual, msg);
     }
 
 }
