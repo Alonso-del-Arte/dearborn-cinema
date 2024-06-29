9ef90050838ce2dbc90d319c00c89b0d2aa8e4e3
Alonso del Arte <alonso.delarte@gmail.com>
2024-06-28 22:16:36 -0400

    Failing chooseDuration( ) test

--- src/test/java/com/dearborncinema/entity/MockMovieDataMakerTest.java
+++ src/test/java/com/dearborncinema/entity/MockMovieDataMakerTest.java
@@ -5,6 +5,7 @@ import static com.dearborncinema.entity.MovieTest.RANDOM;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -41,7 +42,7 @@ public class MockMovieDataMakerTest {
 
     @Test
     void testChooseRating() {
-        MPAARating[] ratings = MPAARating.values();
+        System.out.println("chooseRating");
         Set<MPAARating> expected = new HashSet<>(Set.of(MPAARating.G,
                 MPAARating.PG, MPAARating.PG13, MPAARating.R, MPAARating.NC17));
         int size = expected.size();
@@ -51,6 +52,34 @@ public class MockMovieDataMakerTest {
             actual.add(MockMovieDataMaker.chooseRating());
         }
         assertEquals(expected, actual);
+    }
+
+    // TODO: Write tests for makeUpRatingReason
+
+    // TODO: Rewrite this test to use org.testframe.api.Asserters.assertInRange()
+    @Test
+    void testChooseDuration() {
+        System.out.println("chooseDuration");
+        int minimumMinutes = 77;
+        int maximumMinutes = 150;
+        Duration minimum = Duration.of(minimumMinutes, ChronoUnit.MINUTES);
+        Duration maximum = Duration.of(maximumMinutes, ChronoUnit.MINUTES);
+        int range = maximumMinutes - minimumMinutes;
+        Set<Duration> set = new HashSet<>(range);
+        int numberOfCalls = 16 * range;
+        String msgPart = " should be at least " + minimum.toString() + " and at most " + maximum.toString();
+        for (int i = 0; i < numberOfCalls; i++) {
+            Duration duration = MockMovieDataMaker.chooseDuration();
+            String msg = duration.toString() + msgPart;
+            assert duration.compareTo(minimum) > 0 : msg;
+            assert duration.compareTo(maximum) < 0 : msg;
+            set.add(duration);
+        }
+        int expected = 3 * range / 5;
+        int actual = set.size();
+        String msg = "After " + numberOfCalls + ", expected at least "
+                + expected + " distinct, got " + actual;
+        assert actual >= expected : msg;
     }
 
 }
